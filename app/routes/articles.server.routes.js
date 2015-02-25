ff2185c5a093dfd8a1fa6f5f7895828dfb956d39
Cliff Eby <ebyc@pbworld.com>
2015-02-24 22:56:51 -0500

    Adding Qas Added Modal

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -32,7 +32,7 @@ module.exports = function(app) {
     //    .get(articles.read)
     //    .put(users.requiresLogin, articles.hasAuthorization, articles.update)
     //    .delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
-
+//
     // Finish by binding the article middleware
 	app.param('articleId', articles.articleByID);
 };
